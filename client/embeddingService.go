a32f6f6938acdc2d7c842d8d360fa4ad6c394a7e
lei.gui <550686030@qq.com>
2023-11-09 10:17:13 +0800

    Add limiter function

--- client/embeddingService.go
+++ client/embeddingService.go
@@ -2,37 +2,49 @@ package client
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"io"
+	"math"
 	"net/http"
 
 	"github.com/gui0923/openai-embedding-client/bean"
 	"github.com/pkoukk/tiktoken-go"
+	"golang.org/x/time/rate"
 )
 
 type embeddingClient struct {
-	maxTokens     int
-	maxInputNum   int
-	tiktoken      tiktoken.Tiktoken
-	openaiService openAIEmbeddingProcessService
-	azureService  azureOpenAIEmbeddingProcessService
+	maxTokensPer   int
+	maxInputNumPer int
+	numberLimiter  rate.Limiter
+	tokensLimiter  rate.Limiter
+	tiktoken       tiktoken.Tiktoken
+	openaiService  openAIEmbeddingProcessService
+	azureService   azureOpenAIEmbeddingProcessService
 }
 
 func NewEmbeddingClient(maxTokens int, maxInputNum int) *embeddingClient {
+	return NewLimterEmbeddingClient(maxInputNum, maxInputNum, math.MaxInt32, math.MaxInt32)
+}
+
+func NewLimterEmbeddingClient(maxTokens int, maxInputNum int, requestNumberPerMinute int, requestTokensPerMinute int) *embeddingClient {
 	openaiService := &openAIEmbeddingProcessService{}
 	azureService := &azureOpenAIEmbeddingProcessService{}
 	t, err := tiktoken.GetEncoding("cl100k_base")
 	if err != nil {
 		panic(err)
 	}
-	return &embeddingClient{
-		maxTokens:     maxTokens,
-		maxInputNum:   maxInputNum,
-		tiktoken:      *t,
-		openaiService: *openaiService,
-		azureService:  *azureService,
+	c := &embeddingClient{
+		maxTokensPer:   maxTokens,
+		maxInputNumPer: maxInputNum,
+		tiktoken:       *t,
+		openaiService:  *openaiService,
+		azureService:   *azureService,
 	}
+	c.tokensLimiter = *rate.NewLimiter(rate.Limit(requestTokensPerMinute/60.0), requestTokensPerMinute)
+	c.numberLimiter = *rate.NewLimiter(rate.Limit(requestNumberPerMinute/60.0), requestNumberPerMinute)
+	return c
 }
 
 func (client *embeddingClient) EmbeddingRequest(request *bean.EmbeddingRequest) (bean.EmbeddingResult, error) {
@@ -45,15 +57,23 @@ func (client *embeddingClient) EmbeddingRequest(request *bean.EmbeddingRequest) 
 	for k := range inputMap {
 		request.Input = append(request.Input, k)
 	}
-	if len(request.Input) > client.maxInputNum {
-		return bean.EmbeddingResult{}, errors.New("exceeded maximum input size limit.")
+	if len(request.Input) > client.maxInputNumPer {
+		return bean.EmbeddingResult{}, errors.New("exceeded maximum input size limit")
 	}
 	var inputTotalTokens = 0
 	for _, v := range request.Input {
 		inputTotalTokens += len(client.tiktoken.Encode(v, nil, nil))
 	}
-	if inputTotalTokens >= client.maxTokens {
-		return bean.EmbeddingResult{}, errors.New("exceeded maximum tokens limit.")
+	if inputTotalTokens >= client.maxTokensPer {
+		return bean.EmbeddingResult{}, errors.New("exceeded maximum tokens limit")
+	}
+	err := client.numberLimiter.Wait(context.Background())
+	if err != nil {
+		return bean.EmbeddingResult{}, err
+	}
+	err2 := client.tokensLimiter.WaitN(context.Background(), inputTotalTokens)
+	if err2 != nil {
+		return bean.EmbeddingResult{}, err2
 	}
 	var url string
 	var header map[string]string
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/dlclark/regexp2 v1.10.0 // indirect
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
+	golang.org/x/time v0.4.0 // indirect
 )
